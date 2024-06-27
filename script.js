cd69817aec2143ad7367ad1505d6981c6990097d
Leon <104960228+noel12sirrom@users.noreply.github.com>
2024-06-27 12:07:30 -0400

    "Updated index.html, script.js, and style.css files with various changes to game layout, styling, and functionality."

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let gameOver = false;
+const tiles = document.querySelectorAll(".tile");
+const startButton = document.getElementById("start-button");
+const toggleCorrect = document.getElementById('correct');
+const toggleIncorrect = document.getElementById('incorrect');
+let gameOver = false; //isnt used at yet... theres no reason t use it
 let sequence = [];
 let index = 0; //used to keep track of where in the pattern/sequence user is at
 let wrongTile = '';
+let correctTile ='';
 let gameSpeed = 2000;
 let score = 0;
-//let nextroundStartSpeed = sequence.length * 
 
-const tiles = document.querySelectorAll(".tile");
-const startButton = document.getElementById("start-button");
-
-startButton.addEventListener('click', start);
-//disable until the whole game is over then enable
+startButton.addEventListener('click', start);//once clicked disabled until the gameover
 
 function start(){
     score = 0
@@ -27,6 +27,24 @@ function addNewTileToSequence() {
     sequence.push(Math.floor(Math.random() * 9)); // Use 9 for a 3x3 grid (0-8)
 }
 
+function play(){
+
+        if (wrongTile){
+            wrongTile.classList.remove('wrong-tile');
+            toggleIncorrect.classList.remove('show-incorrect');
+        }
+        if(correctTile){
+            correctTile.classList.remove('correct-tile');
+        }
+
+        tiles.forEach(tile => {
+           tile.classList.add('no-hover');
+        });
+        ;
+        removeTileClickListeners();// Remove any existing listener
+        displayTile();
+}
+
 function displayTile() {
     //alert('called display');
     for (let i = 0; i < sequence.length; i++) {
@@ -35,20 +53,18 @@ function displayTile() {
             setTimeout(() => {
                 tiles[sequence[i]].classList.remove("active");
             }, 1000); 
-        },i* gameSpeed); // Each tile activates 2 second apart
+        },i* gameSpeed); // Each tile activates beased on gamespeed
     }
 
     // After displaying the sequence, wait for the user's input
-    setTimeout(getSequence, sequence.length * gameSpeed);
+    setTimeout(addTileClickListeners, sequence.length * gameSpeed);
 }
 
-function getSequence() {
+function addTileClickListeners() {
     tiles.forEach(tile => {
         tile.addEventListener('click', handleTileClick); // Add the event listener
         tile.classList.remove('no-hover');//LETS YOU KNOW U CAN START CLICKING
     });
-
-    
 }
 
 function handleTileClick(event) {
@@ -61,35 +77,28 @@ function handleTileClick(event) {
             index = 0;//index is at zero because the user is is going to redo pattern from start again
             score += 1;
             addNewTileToSequence();
+            toggleCorrect.classList.add('show-correct')
 
-            if(gameSpeed >800){//increases speed with each level
+            if(gameSpeed >400){//increases speed with each level
                gameSpeed -= 200; 
             }
-            
+
+            setTimeout(()=>{
+                toggleCorrect.classList.remove('show-correct')
+            }, 1500)
             setTimeout(play, 800);
         }
     } else { // Wrong tile clicked
-        tile.classList.add('wrong-tile');
-        wrongTile = tile;
-        gameOver = true;
+        tile.classList.add('wrong-tile');//adds red glow
+        toggleIncorrect.classList.add('show-incorrect');
+        wrongTile = tile;// stores tile so that tile can be reset when game starts, since it's blinking red when gameOver
+        correctTile = document.getElementById('tile-'+ sequence[index]); // connectates 'tile-' to the current tile that is wrong in the sequence 
+        correctTile.classList.add('correct-tile'); //adds green glow
         startButton.disabled = false; // Re-enable the start button
         removeTileClickListeners();
     }
 }
 
-function play(){
-    if (!gameOver) {
-        if (wrongTile){
-            wrongTile.classList.remove('wrong-tile');
-        }
-        tiles.forEach(tile => {
-           tile.classList.add('no-hover');
-        });
-        ;
-        removeTileClickListeners();// Remove any existing listener
-        displayTile();
-    }
-}
 
 function removeTileClickListeners() {
     tiles.forEach(tile => {
