7957ae3e31eb56fd9be7b9b976954e7a680018c5
Leon <104960228+noel12sirrom@users.noreply.github.com>
2024-06-28 18:31:31 -0400

    "Updated index.html, script.js, and style.css files: modified HTML structure, added game score display, changed game logic and styling.>> The Game's main functionality is complete!"

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ const tiles = document.querySelectorAll(".tile");
 const startButton = document.getElementById("start-button");
 const toggleCorrect = document.getElementById('correct');
 const toggleIncorrect = document.getElementById('incorrect');
+const gameScore = document.getElementById('score');
+let gameOver = false;
 let sequence = [];
 let index = 0; //used to keep track of where in the pattern/sequence user is at
 let wrongTile = '';
 let correctTile ='';
-let gameSpeed = 2000;
+let gameSpeed;
 let score = 0;
 
 startButton.addEventListener('click', start);//once clicked disabled until the gameover
 
 function start(){
-    score = 0
+    score = 0;
     index = 0;
-    sequence = [];
+    sequence = [1,1];
     gameOver = false;
     startButton.disabled = true;
+    gameScore.textContent = score;//updates score on screen
     gameSpeed = 2000;
 
     addNewTileToSequence(); 
@@ -27,10 +30,9 @@ function addNewTileToSequence() {
 }
 
 function play(){
-
+        //these reset the color of the displayed 'wrong tile' and 'corrent tile' on game start.
         if (wrongTile){
             wrongTile.classList.remove('wrong-tile');
-            toggleIncorrect.classList.remove('show-incorrect');
         }
         if(correctTile){
             correctTile.classList.remove('correct-tile');
@@ -39,19 +41,25 @@ function play(){
         tiles.forEach(tile => {
            tile.classList.add('no-hover');
         });
-        ;
-        removeTileClickListeners();// Remove any existing listener
+
+        removeTileClickListeners();
         displayTile();
 }
 
 function displayTile() {
-    //alert('called display');
+    let displayPeriod = 1000; //how long a tile is displayed
+
+    //calculates how long a tile should be displayed for based on the speed of the game
+    if(gameSpeed < displayPeriod){
+        displayPeriod = gameSpeed - 100; //there's a 100ms before the next tile shows
+    }
+
     for (let i = 0; i < sequence.length; i++) {
         setTimeout(() => {
             tiles[sequence[i]].classList.add("active");
             setTimeout(() => {
                 tiles[sequence[i]].classList.remove("active");
-            }, 1000); 
+            }, displayPeriod); 
         },i* gameSpeed); // Each tile activates beased on gamespeed
     }
 
@@ -61,7 +69,7 @@ function displayTile() {
 
 function addTileClickListeners() {
     tiles.forEach(tile => {
-        tile.addEventListener('click', handleTileClick); // Add the event listener
+        tile.addEventListener('click', handleTileClick); //alows u to click
         tile.classList.remove('no-hover');//LETS YOU KNOW U CAN START CLICKING
     });
 }
@@ -76,20 +84,25 @@ function handleTileClick(event) {
             index = 0;//index is at zero because the user is is going to redo pattern from start again
             score += 1;
             addNewTileToSequence();
-            toggleCorrect.classList.add('show-correct')
 
-            if(gameSpeed >400){//increases speed with each level
-               gameSpeed -= 200; 
+            if(gameSpeed >200){//increases speed with each level
+                //the transition speed messes with the game eg, tranSITION 0.3s gamespeed 0.1 (tile could be shown 3 times and you wouldnt know because the transition
+                tiles.forEach(tile => {
+                    if(gameSpeed <= 1400){
+                        tile.style.transition = '';
+                    }
+                    else{
+                    tile.style.transition = 'border 0.3s ease-in-out, box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out;';
+                    }
+                });
+
+               gameSpeed -= 200; //game gets faster after each level
             }
-
-            setTimeout(()=>{
-                toggleCorrect.classList.remove('show-correct')
-            }, 1500)
+            gameScore.textContent = score;//updates score on screen 
             setTimeout(play, 800);
         }
     } else { // Wrong tile clicked
         tile.classList.add('wrong-tile');//adds red glow
-        toggleIncorrect.classList.add('show-incorrect');
         wrongTile = tile;// stores tile so that tile can be reset when game starts, since it's blinking red when gameOver
         correctTile = document.getElementById('tile-'+ sequence[index]); // connectates 'tile-' to the current tile that is wrong in the sequence 
         correctTile.classList.add('correct-tile'); //adds green glow
@@ -103,4 +116,3 @@ function removeTileClickListeners() {
         tile.removeEventListener('click',handleTileClick);
     });
 }
-//created new branch
